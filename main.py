1b40b323b979e21ff9fadfbbdf95ac80915f1c07
varmams <varma@srimaharshiconsultanc.com>
2026-06-19 12:51:51 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from printer import print_info
+
+if __name__ == "__main__":
+    print("Calling the printer module:")
+    print_info()
---
+++ printer.py
@@ -0,0 +1,10 @@
+def print_info():
+    name = "John Doe"
+    address = "123 Main Street,\nSpringfield, IL 62701"
+    
+    print(f"Name: {name}")
+    print(f"Address: {address}")
+
+
+if __name__ == "__main__":
+    print_info()
